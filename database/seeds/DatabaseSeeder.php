1aa15eeeee088263ce41a3fe130795716828c67f
John Braun <jbraunnl@gmail.com>
2019-01-27 12:17:15 +0100

    Add database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        $actions = ['collect', 'deliver'];
+
+        $containerColors = [
+            'Blue',
+            'Green',
+            'Blue',
+            'White'
+        ];
+
+        foreach ($actions as $action) {
+            // Insert all liquid waste containers
+            foreach ($containerColors as $color) {
+                \App\Item::create(['type' => $color . ' waste container', 'action' => $action]);
+            }
+            // Insert solid waste containers 
+            \App\Item::create(['type' => 'Solid waste container', 'action' => $action]);
+        }
+
+        \App\Item::create(['type' => 'Liquid nitrogen tank', 'action' => 'deliver']);
+        \App\Item::create(['type' => 'Argon ', 'action' => 'deliver']);
     }
 }
