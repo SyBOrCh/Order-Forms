89edad6776a1249296434a6f753fdf3938c7d8c4
John Braun <jbraunnl@gmail.com>
2019-01-30 12:29:11 +0100

    Small revision of homepage

--- resources/views/home/categories.blade.php
+++ resources/views/home/categories.blade.php
@@ -3,13 +3,13 @@
 @section('content')
     <div class="container">
         
-        <h1 class="text-center mb-4">Categories</h1>
+        <!-- <h3 class="text-center mb-2">Categories</h3> -->
         
         <div class="container mx-auto flex flex-wrap w-full justify-center">
         @foreach ($categories as $category => $items)
         <div class="w-1/4 mb-4 px-2 pt-2 mx-2">
-           <div class="text-center card" style="max-width: 15rem;">
-             <div style="overflow:hidden; height: 200px;">
+           <div class="text-center card" style="max-width: 14rem;">
+             <div style="overflow:hidden; height: 190px;">
                 <img class="card-img-top {{ $currentOrder->contains(\App\Item::where('category', $category)->first()) ? 'grey-out' : '' }}" style="max-height: 190px; overflow:hidden;" src="{{ asset('img/categories/' . str_slug($category) . '.png') }}" >
              </div>
              <div class="card-body text-center">
