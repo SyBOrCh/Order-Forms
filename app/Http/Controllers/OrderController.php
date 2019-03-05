b6aacc423038399674ff40b3f3fe811be060e97b
John Braun <jbraunnl@gmail.com>
2019-03-05 11:23:14 +0100

    Reverse sort the Orders

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -20,7 +20,7 @@ class OrderController extends Controller
     public function index()
     {
         // $orders = Auth::user()->closedOrders();
-        $orders = Order::where('open', false)->get();
+        $orders = Order::where('open', false)->latest()->get();
         return view('orders.index', compact('orders'));
     }
 
